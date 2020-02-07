cc86257a0ce1fc852502fb1582abeddf54a55a2a
Ian Cook <icook@cloudera.com>
2020-02-07 19:08:53 +0000

    Add Markdown examples

---
+++ sessions/markdown.py
@@ -0,0 +1,44 @@
+# Copyright 2020 Cloudera, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# # Markdown
+
+# Comments in Python code files in CML can 
+# use Markdown syntax. For example:
+
+# # Heading 1
+
+# ## Heading 2
+
+# ### Heading 3
+
+# Plain text
+
+# *Emphasized text* or _Emphasized text_
+
+# **Bold text**
+
+# `Monospaced text`
+
+# Bulleted list
+# * Item
+#   * Subitem
+# * Item
+
+# Numbered list
+# 1. First Item
+# 2. Second Item
+# 3. Third Item
+
+# [link](https://www.cloudera.com)
