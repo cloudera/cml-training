dc5cd5c37c7a4c98d2e5bd4b4e79338ecdc2d3f1
Ian Cook <icook@cloudera.com>
2020-05-27 19:48:25 +0000

    Add Models examples

--- models/model.R
+++ models/model.R
@@ -1,2 +1,31 @@
-# This file will contain example R code
-# to demonstrate Models in CML
+# Copyright 2020 Cloudera, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# # Models Example with tidymodels
+
+# Load required packages:
+library(dplyr)
+
+# Load the trained model:
+model <- readRDS("model.rds")
+
+# Define a function that can be called to generate
+# predictions from the model:
+pred_arr_delay <- function(args) {
+  dep_delay <- args$dep_delay
+  prediction <- model %>%
+    predict(list(dep_delay = dep_delay)) %>%
+    pull(.pred)
+  list(pred_arr_delay = prediction)
+}
--- models/model.py
+++ models/model.py
@@ -1,3 +1,28 @@
-# This file will contain example Python code
-# to demonstrate Models in CML
+# Copyright 2020 Cloudera, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# # Models Example with scikit-learn
+
+# Import required modules:
+from joblib import load
+
+# Load the trained model:
+model = load('model.joblib')
+
+# Define a function that can be called to generate
+# predictions from the model:
+def pred_arr_delay(args):
+  dep_delay = args["dep_delay"]
+  pred_arr_delay = model.predict([[dep_delay]]).item()
+  return {"pred_arr_delay": pred_arr_delay}
